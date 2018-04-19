c93067146e949ad7edc717cc1531c5710d9eb82f
jelizondo <jelizondolevy.s@gmail.com>
2018-04-19 10:43:51 +0200

    Project setup

---
+++ src/components/CryptoCard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './CryptoCard.css';
+
+class CryptoCard extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default CryptoCard;
---
+++ src/components/CurrencyContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './CurrencyContainer.css';
+
+class CurrencyContainer extends Component {
+  render() {
+    return (
+      <div>
+        <h1> CurrencyContainer </h1>
+      </div>
+    );
+  }
+}
+
+export default CurrencyContainer;
